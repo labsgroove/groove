96267b5fae3fe355c2af52d7bb2c0c77acf33463
Groove Labs <145621708+labsgroove@users.noreply.github.com>
2026-06-26 13:01:34 -0300

    up

--- [qb]/qb-treta/config.lua
+++ [qb]/qb-treta/config.lua
@@ -2,14 +2,14 @@ Config = {}
 
 Config.Gangs = {
     gangA = {
-        pedModels = {"a_m_m_afriamer_01", "a_m_m_genfat_01"},
-        weapons = {"weapon_unarmed"},
+        pedModels = {"s_m_y_clown_01"},
+        weapons = {"weapon_candycane"},
         count = 5,
         coords = vector3(-1272.9287, -1519.9923, 4.3136)
     },
     gangB = {
-    pedModels = {"a_m_m_fatlatin_01", "a_m_m_farmer_01"},
-        weapons = {"weapon_unarmed"},
+    pedModels = {"s_m_y_mime"},
+        weapons = {"weapon_bread"},
         count = 5,
         coords = vector3(-1277.9823, -1541.3083, 4.3137)
     }
